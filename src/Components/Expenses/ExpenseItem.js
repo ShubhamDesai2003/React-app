d2e17228e2facfbfe3028c5d251112410a59761c
Shubham <shubhamdesai20003@gmail.com>
2023-07-26 15:44:32 +0530

    useState function

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 
@@ -8,14 +8,23 @@ import Nest from './nest';
 
 const ExpenseItem = (props) =>{
 
+    
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = ()=>{
+        setTitle('New Title');
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date = {props.date} />
             <Nest className='expense-item__description'>
 
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </Nest>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
